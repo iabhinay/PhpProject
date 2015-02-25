3792bd9edf1d5f0726bc726268a3a4d3a42a7f9b
saisoft <saisoft@php-linux>
2015-02-25 00:38:30 -0500

    added contents

--- helloworld.php
+++ helloworld.php
@@ -1,5 +1,4 @@
-<?php
-
+ <?php
 /* 
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -7,11 +6,8 @@
  */
 
 // This will be hello world
-
-$var = 'hello'; //this sets hello world
-
-function helloworld($hi) {
-
-    echo $hi;
-}
-helloworld($hi);
+$string1 = "hello";
+$string2 = "world";
+$string3 = "$string1 $string2";
+$string4 = "hi this is $string3";
+echo $string4;
